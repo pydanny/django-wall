5ea73ab23b645320bb2a8a70b3a0e0e65953384d
Rock Howard <rockmhoward@gmail.com>
2009-02-25 23:18:55 +0000

    recent_wall now also accepts a slug as a parameter instead of only accepting a wall instance

--- wall/templatetags/walltags.py
+++ wall/templatetags/walltags.py
@@ -1,5 +1,6 @@
 import datetime
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from django import template
 from django.core.urlresolvers import reverse
 from wall.models import Wall
@@ -8,6 +9,14 @@ register = template.Library()
 
 @register.inclusion_tag("wall/_recent_wall.html")
 def recent_wall( wall, amount=5, days=7 ):
+    """ Inclusion tag that displays a wall object (or a wall that
+        is procured via the supplied slug.) Only recent items are
+        displayed according to the addtional parameters:
+            -- amount - maximum number of items to display
+            -- days - items must be no more than this number of days old.
+    """
+    if not hasattr( wall, "get_recent_items" ):
+        wall = get_object_or_404( Wall, slug=wall )
     items = wall.get_recent_items( amount, days )
     number = len(items)
     if number == 0:
